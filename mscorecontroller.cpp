e33e6a28d642b8e6efab4314bc17899c03716cf3
Werner Schweer <ws@mecky.(none)>
2013-02-03 11:39:16 +0100

    add build instructions; add jpeg format

--- mscorecontroller.cpp
+++ mscorecontroller.cpp
@@ -74,6 +74,112 @@ static void processPng(HttpResponse& response, Score* score)
       response.write(ba, true);
       }
 
+//---------------------------------------------------------
+//   processJpeg
+//---------------------------------------------------------
+
+static void processJpeg(HttpResponse& response, Score* score)
+      {
+      qreal convDpi = 100.0;
+      score->setPrinting(true);
+
+      QImage::Format f = QImage::Format_ARGB32_Premultiplied;
+
+      const QList<Page*>& pl = score->pages();
+
+      Page* page = pl.at(0);
+
+      QRectF r = page->abbox();
+      int w = lrint(r.width()  * convDpi / MScore::DPI);
+      int h = lrint(r.height() * convDpi / MScore::DPI);
+
+      QImage printer(w, h, f);
+      printer.setDotsPerMeterX(lrint((convDpi * 1000) / INCH));
+      printer.setDotsPerMeterY(lrint((convDpi * 1000) / INCH));
+
+      printer.fill(0xffffffff);
+
+      double mag = convDpi / MScore::DPI;
+      QPainter p(&printer);
+
+      p.setRenderHint(QPainter::Antialiasing, true);
+      p.setRenderHint(QPainter::TextAntialiasing, true);
+      p.scale(mag, mag);
+
+      foreach(const Element* e, page->elements()) {
+            if (!e->visible())
+                  continue;
+            QPointF pos(e->pagePos());
+            p.translate(pos);
+            e->draw(&p);
+            p.translate(-pos);
+            }
+      score->setPrinting(false);
+      QByteArray ba;
+      QBuffer buffer(&ba);
+      buffer.open(QIODevice::WriteOnly);
+      if (!printer.save(&buffer, "JPEG"))
+            printf("   export failed\n");
+      response.setHeader("Content-Type", "image/jpeg");
+      response.write(ba, true);
+      }
+
+//---------------------------------------------------------
+//   processGif
+//---------------------------------------------------------
+
+static void processGif(HttpResponse& response, Score* score)
+      {
+      printf("process gif...\n");
+      qreal convDpi = 100.0;
+      score->setPrinting(true);
+
+      QImage::Format f = QImage::Format_ARGB32_Premultiplied;
+
+      const QList<Page*>& pl = score->pages();
+
+      Page* page = pl.at(0);
+
+      QRectF r = page->abbox();
+      int w = lrint(r.width()  * convDpi / MScore::DPI);
+      int h = lrint(r.height() * convDpi / MScore::DPI);
+
+      QImage printer(w, h, f);
+      printer.setDotsPerMeterX(lrint((convDpi * 1000) / INCH));
+      printer.setDotsPerMeterY(lrint((convDpi * 1000) / INCH));
+
+      printer.fill(0xffffffff);
+
+      double mag = convDpi / MScore::DPI;
+      QPainter p(&printer);
+
+      p.setRenderHint(QPainter::Antialiasing, true);
+      p.setRenderHint(QPainter::TextAntialiasing, true);
+      p.scale(mag, mag);
+
+      foreach(const Element* e, page->elements()) {
+            if (!e->visible())
+                  continue;
+            QPointF pos(e->pagePos());
+            p.translate(pos);
+            e->draw(&p);
+            p.translate(-pos);
+            }
+      score->setPrinting(false);
+      QByteArray ba;
+      QBuffer buffer(&ba);
+      buffer.open(QIODevice::WriteOnly);
+      if (!printer.save(&buffer, "GIF"))
+            printf("   export failed\n");
+      response.setHeader("Content-Type", "image/gif");
+      printf("   respond %d\n", ba.size());
+      response.write(ba, true);
+      }
+
+//---------------------------------------------------------
+//   service
+//---------------------------------------------------------
+
 void MScoreController::service(HttpRequest& request, HttpResponse& response)
       {
       QByteArray path = request.getPath();
@@ -112,9 +218,12 @@ void MScoreController::service(HttpRequest& request, HttpResponse& response)
             // process score
             //
             QByteArray format = request.getParameter("format");
-            if (format == "png") {
+            if (format == "png")
                   processPng(response, score);
-                  }
+            else if (format == "jpeg")
+                  processGif(response, score);
+            else if (format == "gif")
+                  processGif(response, score);
             else {
                   printf("bad format <%s>\n", qPrintable(format));
                   return;
